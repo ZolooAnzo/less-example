778d8bb5c9887e739ab7aa03790d9dfe9d7624d4
ZolooAnzo <zolooanzo@gmail.com>
2021-11-15 15:07:11 +0800

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.less';
 export default function App() {
   return (
     <header className="header">
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>CSS LESS</h1>
+      <p>hud hicheel number 1</p>
+      <button className="button button-ok">OK</button>
+      <button className="button button-cancel">CANCEL</button>
     </header>
   );
 }
